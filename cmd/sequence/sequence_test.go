78a95c997c0a0f58be7b0dca83f9c9c2dff79e9f
KennethanCeyer <kenneth@pigno.se>
2025-09-07 22:10:12 +0900

    fix: extend timeout for preventing error

--- cmd/sequence/sequence_test.go
+++ cmd/sequence/sequence_test.go
@@ -17,7 +17,7 @@ func TestSequence_NormalCompletion(t *testing.T) {
 		t.Skip("skipping integration test in short mode")
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, "go", "run", ".")
